a0dc56d150ab2479470004a5095ac45c2f071a8e
Riversity <maayihjia@sjtu.edu.cn>
2024-10-08 22:43:17 +0800

    Fix carry to real carry. Finish.

--- adder/adder-carry.v
+++ adder/adder-carry.v
@@ -13,8 +13,13 @@ module adder(
     for(i = 0; i <= 15; i = i + 1) begin
       assign G[i] = a[i] & b[i];
       assign P[i] = a[i] ^ b[i];
-      assign c[i+1] = G[i] | P[i] & c[i];
       assign sum[i] = P[i] ^ c[i];
+    end
+    for(i = 0; i <= 3; i = i + 1) begin
+      assign c[4*i+1] = G[4*i] | P[4*i] & c[4*i];
+      assign c[4*i+2] = G[4*i+1] | P[4*i+1] & G[4*i] | P[4*i+1] & P[4*i] & c[4*i];
+      assign c[4*i+3] = G[4*i+2] | P[4*i+2] & G[4*i+1] | P[4*i+2] & P[4*i+1] & G[4*i] | P[4*i+2] & P[4*i+1] & P[4*i] & c[4*i];
+      assign c[4*i+4] = G[4*i+3] | P[4*i+3] & G[4*i+2] | P[4*i+3] & P[4*i+2] & G[4*i+1] | P[4*i+3] & P[4*i+2] & P[4*i+1] & G[4*i] | P[4*i+3] & P[4*i+2] & P[4*i+1] & P[4*i] & c[4*i];
     end
   endgenerate
   assign carry = c[16];
